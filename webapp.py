6d5b5de80a95c17526682a2011bd3c386f5ed90f
Ajinkya Patil <35138923+patilajinkya@users.noreply.github.com>
2025-06-16 16:38:31 +0530

    Addeed comments for better readability

--- webapp.py
+++ webapp.py
@@ -7,7 +7,6 @@ app = Flask(__name__) # Created a flask instance
 @app.route('/random', methods=['GET']) # Mapped /random endpoint path to <get_data> function.
 def get_data():
     random_number = random_number_generator(existing_numbers)    # Generating a random number by providing list of already generated number to exclude
-    print(random_number)
     mysql_insert_random_generated_numbers(random_number)  # Inserting a number to mysql DB to make it persistent.
     data = {'random':random_number}
 
